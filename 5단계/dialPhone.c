81c2c082b277f97530e4f49d7e66009714b679e5
이민성 <133635509+minsung-phy@users.noreply.github.com>
2023-12-29 14:25:03 +0900

    Add files via upload

---
+++ 5단계/dialPhone.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+	int time = 0;
+	char arr[16] = {0, };
+	scanf("%s", arr);
+	
+	for (int i = 0; i < 15; i++) {
+		if (arr[i] != 0) {
+			switch (arr[i]) {
+				case 'A': case 'B': case 'C':
+					time += 3;
+					break;
+				case 'D': case 'E': case 'F':
+					time += 4;
+					break;
+				case 'G': case 'H': case 'I':
+					time += 5;
+					break;
+				case 'J': case 'K': case 'L':
+					time += 6;
+					break;
+				case 'M': case 'N': case 'O':
+					time += 7;
+					break;
+				case 'P': case 'Q': case 'R': case 'S':
+					time += 8;
+					break;
+				case 'T': case 'U': case 'V':
+					time += 9;
+					break;
+				case 'W': case 'X': case 'Y': case 'Z':
+					time += 10;
+					break;
+			}
+		}
+	}
+	printf("%d\n", time);
+
+	return 0;
+}
+
